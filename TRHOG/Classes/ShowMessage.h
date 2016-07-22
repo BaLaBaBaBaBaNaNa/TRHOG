4b0a88d7e3c077128e6a4ec5fff054cb40e14f20
BaLaBaBaBaBaNaNa <2658142829@qq.com>
2016-07-22 13:25:12 +0800

    提交那个更改

---
+++ TRHOG/Classes/ShowMessage.h
@@ -0,0 +1,13 @@
+//
+//  ShowMessage.h
+//  Pods
+//
+//  Created by Dreamsleep on 16/7/22.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShowMessage : NSObject
+- (void)showMessage;
+@end
---
+++ TRHOG/Classes/ShowMessage.m
@@ -0,0 +1,16 @@
+//
+//  ShowMessage.m
+//  Pods
+//
+//  Created by Dreamsleep on 16/7/22.
+//
+//
+
+#import "ShowMessage.h"
+
+@implementation ShowMessage
+- (void)showMessage
+{
+    NSLog(@"HELLO POD");
+}
+@end
